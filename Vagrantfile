d9229e9389ab0d151436a623e719a5b76fb42f79
uniuuu <alex.kred@yahoo.co.uk>
2019-09-12 19:21:02 +0800

    fix reboot

---
+++ Vagrantfile
@@ -0,0 +1,26 @@
+IMAGE_NAME = "dongsupark/coreos-stable"
+N = 1
+
+Vagrant.configure("2") do |config|
+    config.ssh.insert_key = false
+
+    config.vm.provider "libvirt" do |v|
+        v.memory = 2048
+        v.cpus = 2
+        v.driver = "kvm"
+        v.uri = "qemu:///system"
+        v.qemu_use_session = false
+    end
+      
+    config.vm.define "testing" do |master|
+        master.vm.box = IMAGE_NAME
+        master.vm.network "private_network", ip: "10.10.50.10"
+        master.vm.hostname = "k8s-master"
+        master.vm.provision "ansible" do |ansible|
+            ansible.playbook = "kubernetes-setup/master-playbook.yml"
+            ansible.extra_vars = {
+                node_ip: "10.10.50.10",
+            }
+        end
+    end
+ end
---
+++ kubernetes-setup/kubeadm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o nounset -o errexit
+
+
+CNI_VERSION="v0.7.5"
+mkdir -p /opt/cni/bin
+curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-amd64-${CNI_VERSION}.tgz" | tar -C /opt/cni/bin -xz
+
+CRICTL_VERSION="v1.12.0"
+mkdir -p /opt/bin
+curl -L "https://github.com/kubernetes-incubator/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz" | tar -C /opt/bin -xz
+
+RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+
+mkdir -p /opt/bin
+cd /opt/bin
+curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
+chmod +x {kubeadm,kubelet,kubectl}
+
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/kubelet.service" | sed "s:/usr/bin:/opt/bin:g" > /etc/systemd/system/kubelet.service
+mkdir -p /etc/systemd/system/kubelet.service.d
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/10-kubeadm.conf" | sed "s:/usr/bin:/opt/bin:g" > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
